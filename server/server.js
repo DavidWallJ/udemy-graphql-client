91f8bf1d0709e61e46b62739631e568985c3de65
DavidWallJ <djkindread@gmail.com>
2017-07-01 17:44:35 +0800

    apollo is setup

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://davidjwall:Meiguanxi8@ds117931.mlab.com:17931/davidjwalllyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
